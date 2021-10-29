11138e151aea2276eaaa901670d4958a38fa3f38
KacoPC <kacperrtv@gmail.com>
2021-10-29 22:16:03 +0200

    session fix

--- navibar.php
+++ navibar.php
@@ -51,6 +51,7 @@
                                  <div class='author-author__info inline has_dropdown'>
 
                                      <?php
+                                     session_start();
                                         // Check if the user is already logged in, if yes then redirect him to welcome page
                                         if (isset($_SESSION['loggedin']) && @($_SESSION['loggedin'] == true)) {
                                             echo "
