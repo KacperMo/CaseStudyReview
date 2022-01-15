77f15ebce9e4730b1150323895aa5c8074922fc6
kamil-ciesla <kamil.ciesla.dev@gmail.com>
2022-01-16 00:33:03 +0100

    change database naming convention to snake_case

--- sql/main.sql
+++ sql/main.sql
@@ -11,49 +11,49 @@ USE `csr`;
 
 -- Create required tables.
 CREATE TABLE `comments`(
-    `commentID` int(32) AUTO_INCREMENT PRIMARY KEY,
-    `publicationID` int(32) NOT NULL,
-    `commentContent` varchar(1023) NOT NULL,
-    `commentAuthor` varchar(255) NOT NULL
+    `comment_id` int(32) AUTO_INCREMENT PRIMARY KEY,
+    `publication_id` int(32) NOT NULL,
+    `comment_content` varchar(1023) NOT NULL,
+    `comment_author` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `authorization_data`(
-    `userID` int(32) AUTO_INCREMENT PRIMARY KEY,
-    `username` varchar(255) NOT NULL UNIQUE,
+    `user_id` int(32) AUTO_INCREMENT PRIMARY KEY,
+    `user_name` varchar(255) NOT NULL UNIQUE,
     `password` varchar(255) NOT NULL,
-    `registrationDate` date DEFAULT CURRENT_DATE(),
-    `emailAddress` varchar(255) NOT NULL UNIQUE,
-    `userPermissions` varchar(255) NOT NULL,
-    `lastLoginDate` date NOT NULL
+    `registration_date` date DEFAULT CURRENT_DATE(),
+    `user_email` varchar(255) NOT NULL UNIQUE,
+    `user_permissions` varchar(255) NOT NULL,
+    `last_login_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `publications` (
-    `publicationID` int(32) AUTO_INCREMENT PRIMARY KEY,
-    `isPublic` tinyint(1) NOT NULL,
-    `authorID` int(32) NOT NULL,
-    `caseSupervisor` varchar(255) DEFAULT NULL,
-    `submissionDate` date DEFAULT CURRENT_DATE(),
+    `publication_id` int(32) AUTO_INCREMENT PRIMARY KEY,
+    `is_public` tinyint(1) NOT NULL,
+    `author_id` int(32) NOT NULL,
+    `case_supervisor` varchar(255) DEFAULT NULL,
+    `submission_date` date DEFAULT CURRENT_DATE(),
     `title` varchar(255) NOT NULL,
     `category` varchar(31) NOT NULL,
     `views` int(32) NOT NULL,
-    `thumbnailPath` varchar(255) DEFAULT NULL,
-    `publicationPath` varchar(255) NOT NULL,
+    `thumbnail_path` varchar(255) DEFAULT NULL,
+    `publication_path` varchar(255) NOT NULL,
     `description` varchar(1023) NOT NULL,
     `rating` float(3,2) NOT NULL
  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `users` (
-    `userID` int(32) NOT NULL,
-    `firstName` varchar(63) NOT NULL,
-    `lastName` varchar(127) NOT NULL,
+    `user_id` int(32) NOT NULL,
+    `first_name` varchar(63) NOT NULL,
+    `last_name` varchar(127) NOT NULL,
     `college` varchar(255) DEFAULT NULL,
-    `avatarPath` varchar(255) DEFAULT NULL,
-    `birthDate` date NOT NULL,
+    `avatar_path` varchar(255) DEFAULT NULL,
+    `birth_date` date NOT NULL,
     `description` varchar(1023) DEFAULT NULL,
     `country` varchar(255) NOT NULL,
     `website` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Create admin user.
-INSERT INTO `authorizationData` (`username`, `password`, `emailAddress`) VALUES
+INSERT INTO `authorization_data` (`user_name`, `password`, `user_email`) VALUES
 ('admin', 'admincsr', 'hi@casestudyreview.com');
